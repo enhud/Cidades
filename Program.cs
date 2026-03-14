1580ea3df647d2ac474396ff6ae91a49915e94c9
joao <joaopedroazevedo1980@gmail.com>
2026-03-13 21:59:55 -0300

    Cidades do litoral sul

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Cananéia");
+Console.WriteLine("Ilha Comprida");
+Console.WriteLine("Iguape");
+Console.WriteLine("Peruíbe");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("São Vicente");
+Console.WriteLine("Santos");
+Console.WriteLine("Guarujá");
+Console.WriteLine("Bertioga");
